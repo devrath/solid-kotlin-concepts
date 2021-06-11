f82fbf180c136d034b3a002b8ac7e880fc47ea26
Devrath <devrath.dev595@gmail.com>
2021-06-11 23:58:09 +0530

    xss

---
+++ projects/BasicsPart1/src/main/kotlin/DemoNullable.kt
@@ -0,0 +1,18 @@
+class DemoNullable {
+
+    init { initiate() }
+
+    private fun initiate() {
+        var valueOne : String  = "FirstName"
+        var valueTwo : String?  = null
+        println(valueTwo)
+
+
+        var valueThree : String  = "FirstName"
+        var valueFour : String?  = null
+        if(valueFour!!.length==2){
+            println("Success")
+        }
+    }
+
+}
