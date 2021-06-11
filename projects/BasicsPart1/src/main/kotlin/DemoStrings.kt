e17948ae2220039f6b34a1ebe7e4ee9f21022f4b
Devrath <devrath.dev595@gmail.com>
2021-06-11 18:36:31 +0530

    d

--- projects/BasicsPart1/src/main/kotlin/DemoStrings.kt
+++ projects/BasicsPart1/src/main/kotlin/DemoStrings.kt
@@ -1,2 +1,16 @@
 class DemoStrings {
+
+    init { initiate() }
+
+    private fun initiate() {
+        // Assigning explicitly the data type and then setting a value to it
+        // This will always accept only the integer
+        var age : Int = 21
+        var firstName : String = "Tony"
+        // Kotlin can infer the values so that we need not have to explicitly mention them, Meaning datatype , we need not have to add
+        var lastName : String = "Stark"
+        println("My name is $firstName $lastName and I am $age years old scientist")
+
+    }
+
 }
